2c66750894d451744f86f39c7c87867b652f648b
Okunlola Muhammad Toyeeb <96062255+devExponent@users.noreply.github.com>
2025-10-28 10:26:56 +0200

    My blog Post

--- src/Component/Post.jsx
+++ src/Component/Post.jsx
@@ -4,14 +4,46 @@ import { Api } from "../Services/Api";
 import { useState, useEffect } from "react";
 
 const Post = () => {
+  const [post, setPost] = useState([]);
+  const [isLoading, setIsLoading] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      setIsLoading(true);
+      try {
+        const res = await Api.get("?_limit=5");
+
+        setPost(res.data);
+        console.log(res.data);
+      } catch (error) {
+        console.log(error);
+        setError("Failed to fetch post");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchPost();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading posts...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
   return (
-    <div>
-      {/* <Card
-        title="Noteworthy technology acquisitions 2021"
-        body="  Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological order"
-      /> */}
-    </div>
+    <section className="w-10/12 mx-auto">
+      <div className="flex gap-4">
+        {post.map((users) => (
+          <div key={users.id}>
+            <Card title={users.title} body={users.body} />
+          </div>
+        ))}
+      </div>
+    </section>
   );
 };
 
